bafa4960f30a338ee1505e246d8232be0168851a
Billy <alexandernguyenbill@gmail.com>
2024-01-10 10:26:37 +0700

    add new activity

---
+++ app/src/main/java/rmit/ad/rmeet_dating_app/MainActivity.java
@@ -0,0 +1,14 @@
+package rmit.ad.rmeet_dating_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
